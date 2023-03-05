5a864863e0c239dcad6a5707460ca26a5f980dd2
RBryan05 <126432796+RBryan05@users.noreply.github.com>
2023-03-04 21:24:43 -0600

    Update actividadAsincrona – Semana06(Grupo).py

---
+++ actividadAsincrona – Semana06(Grupo).py
@@ -0,0 +1,19 @@
+# Actividad asincrona semana 6, codigo.
+# Suma: Sumar 3 variables distintas y asignar el valor a una cuarta variable.
+sumaNum1 = 35
+sumaNum2 = 19
+sumaNum3 = 29
+suma = sumaNum1 + sumaNum2 + sumaNum3 
+
+# concatenación con: f-string
+print(f"El resultado de la suma de {sumaNum1} + {sumaNum2} + {sumaNum3} es igual a: {suma}")
+
+#Resta: Restar 4 variables siendo en la tercera variable más alta que la primera.
+restaNum1 = 30
+restaNum2 = 16
+restaNum3 = 34
+restaNum4 = 12
+resta = restaNum1 - restaNum2 - restaNum3 - restaNum4
+
+# concatenación con: f-string
+print(f"El resultado de la resta de {restaNum1} - {restaNum2} - {restaNum3} - {restaNum4} es igual a: {resta}")
